45e1f9bf6d08d2086c06e6b2edeea94f0cc3bce9
NF RIDOY <nfridoy@gmail.com>
2023-09-16 19:58:22 +0600

    credit must be equal and less then 20 is now functional

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,23 +3,60 @@ import Cards from "./cards/Cards"
 import Cart from "./cart/Cart"
 export default function Main() {
     const [selectCourses, setSelectCourse] = useState([]);
-    const [credit, setCredit] = useState(0);
+    // const [totalCredit, setTotalCredit] = useState(0);
+
+    // let totalCredit = 0
     const [remaining, setRemaining] = useState(0);
     // const [remaining, setRemaining] = useState(0);
+
     const handleSelectBtn = (newSelectCourse) => {
         // const newSelectCourse = [...selectCourse];
         // console.log(newSelectCourse);
         const isExist = selectCourses.find(selectCourse => selectCourse.id == newSelectCourse.id)
-        { console.log(isExist) }
-        if(isExist) {
-        // if (!selectCourses.includes(newSelectCourse)) {
-            return alert("already Booked");
+        // console.log(totalCredit + newSelectCourse.credit)
+        // setTotalCredit(totalCredit + newSelectCourse.credit)
+        // totalCredit = newSelectCourse.credit
+        
+        
+        // totalCredit = totalCredit + newSelectCourse.credit;
+        let count = newSelectCourse.credit ; //1
+        // setTotalCredit(newSelectCourse.credit)//2
+        if (isExist) {
+            // if (!selectCourses.includes(newSelectCourse)) {
+            return alert("Already Selected");
         }
         else {
-            const newSelectCourseList = [...selectCourses, newSelectCourse];
-            setSelectCourse(newSelectCourseList);
-            // setSelectCourse([...selectCourses]);
+            selectCourses.forEach((course) => {
+                count = count + course.credit; //1
+                // setTotalCredit(totalCredit + course.credit) //2
+                // console.log(count);
+
+            })
+            if(count <= 20){ //1
+            // if (totalCredit <= 20) { //2
+                setSelectCourse([...selectCourses, newSelectCourse]);
+
+            }
+            else {
+                return alert("Credit Overload")
+            }
         }
+        // else if (totalCredit <= 20) {
+
+
+        //     if (totalCredit > 20) {
+        //         return alert("Total Credit Exiced");
+        //     }
+        //     else {
+        //         const newSelectCourseList = [...selectCourses, newSelectCourse];
+        //         setSelectCourse(newSelectCourseList);
+        //     }
+
+        //     // setSelectCourse([...selectCourses]);
+        // }
+        // else{
+        //     return alert("Overload");
+        // }
         // setSelectCourse(newSelectCourse);
 
     }
